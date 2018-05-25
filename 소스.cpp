445e80fe4c1a2b872a6619ed5f4095ece45d8f82
SeungHuLee <gadlsh12081208@gmail.com>
2018-05-25 20:12:29 +0900

    Initial commit

---
+++ 소스.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int input1 = 0;
+	int input2 = 0;
+	char input3;
+	cout << "Input number 1 : "; 
+	cin >> input1;
+	cout << "Input number 2 : ";
+	cin >> input2;
+	cout << "choose operation : ";
+	cin >> input3;
+
+	switch (input3) {
+	case '+':
+		cout << input1 + input2 << endl;
+		break;
+	}
+
+
+}
